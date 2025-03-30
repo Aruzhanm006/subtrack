3a914c061f8850d1165e446c6ccf1ba68761f2a3
Aruzhanm006 <aruoskenbaeva@gmail.com>
2025-03-30 19:27:03 +0500

    models

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Subscription(models.Model):
+    SERVICE_CHOICES = [
+        ('Netflix', 'Netflix'),
+        ('Spotify', 'Spotify'),
+        ('YouTube', 'YouTube Premium'),
+        ('Other', 'Басқа'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    service_name = models.CharField(max_length=100, choices=SERVICE_CHOICES)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    payment_date = models.DateField()
+    reminder_days_before = models.IntegerField(default=1)  # ескерту күні
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return f"{self.service_name} ({self.user.username})"
