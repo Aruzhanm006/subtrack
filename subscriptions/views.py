9d86ae558bf4b548a8c5ae8eac7e68b5d8b0abd2
Aruzhanm006 <aruoskenbaeva@gmail.com>
2025-03-30 19:38:45 +0500

    dashboard

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .models import Subscription
+
 
 def index(request):
     return render(request, 'subscriptions/index.html')
+
+
+def dashboard(request):
+    if request.user.is_authenticated:
+        subscriptions = Subscription.objects.filter(user=request.user)
+        return render(request, 'subscriptions/dashboard.html', {'subscriptions': subscriptions})
+    else:
+        return render(request, 'subscriptions/unauthorized.html')
